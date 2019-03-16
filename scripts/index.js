61719a3c4db5b45e197462442718449f9e0f823e
Rebecca Uranga <urangarebecca@gmail.com>
2019-03-16 13:53:48 -0400

    tweaks search bar css

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 
-var video = document.querySelector("fullscreen-bg");
 
 async function fetchMoviesFromUserInput(searchQuery) {
     const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=4e818663c4f334a33277fd88c377dea4&language=en-US&query=${searchQuery}&page=1&include_adult=false`
@@ -8,13 +7,18 @@ async function fetchMoviesFromUserInput(searchQuery) {
     return searchResults;
 }
 
+console.log(fetchMoviesFromUserInput('bambi'));
+
 async function movieSearch()  {
     const inputElement = document.querySelector("input");
+    // window.location = `/movie-listing.html?query=${inputElement.value}`;
     if (inputElement.value) {
         const response = await fetchMoviesFromUserInput(inputElement.value);
         const { results } = response; //destructuring assignment the results array from the response 
         renderMovieResults(results); // TODO: render these movies into the DOM
         hideVideo();
+        hideHeader();
+        
     } else {
         alert('Please enter a movie!');
     }
@@ -22,9 +26,9 @@ async function movieSearch()  {
 
 /** render a single search result */
 function renderMovieResults(movies) {
+    /** get parent (container) element that will hold the list of movies */
     movies.forEach(movie => {
         console.log(movie);
-
         const titleElement = document.createElement("h1");
         titleElement.textContent = movie.title; 
         const detailArea = document.querySelector("[data-details]");
@@ -37,13 +41,20 @@ function renderMovieResults(movies) {
         const moviePosterElement = document.createElement("img");
         moviePosterElement.setAttribute('src', `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`);
         detailArea.appendChild(moviePosterElement);
-
     });
 }
+
 
 function hideVideo() {
     const videoElement = document.querySelector(".fullscreen-bg");
     videoElement.style.display = "none";
 }
+
+function hideHeader() {
+    const headerElement = document.querySelector(".header");
+    headerElement.style.display = "none";
+}
+
+
 
 
