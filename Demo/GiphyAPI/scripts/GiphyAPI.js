c99725f1fc63760e28be5fb92c918670f2601c5d
iTrauco <Chris@Trau.co>
2019-03-18 14:14:48 -0400

    Search functionality of MovieDB > Index.html and GiphyAPI > index.html added

--- Demo/GiphyAPI/scripts/GiphyAPI.js
+++ Demo/GiphyAPI/scripts/GiphyAPI.js
@@ -1,6 +1,6 @@
 (function () {
   function giphySearch(keyword) {
-    return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${GIPHY_KEY}&limit=12`)
+    return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${GIPHY_KEY}&limit=4`)
       .then(response => response.json());
   }
 
