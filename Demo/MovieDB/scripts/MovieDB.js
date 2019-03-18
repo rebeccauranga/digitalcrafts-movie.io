9ed8ffdb4e9c6fed646dd25913c2b049112397c5
Rebecca Uranga <urangarebecca@gmail.com>
2019-03-18 15:33:55 -0400

    creates only one form to search movies and gifs

--- Demo/MovieDB/scripts/MovieDB.js
+++ Demo/MovieDB/scripts/MovieDB.js
@@ -1,3 +1,5 @@
+console.log('lordt help me');
+
 async function fetchMoviesFromUserInput(searchQuery) {
     const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=4e818663c4f334a33277fd88c377dea4&language=en-US&query=${searchQuery}&page=1&include_adult=false`
     const response = await fetch(API_URL);
